b6241eb30766a91a63b2cfadbf227357a43cad35
jpinkman <floriano.webdev@gmail.com>
2026-03-12 00:33:39 +0100

    Add 1st ex

---
+++ chapter_00/ex00/Hello.py
@@ -0,0 +1,10 @@
+ft_list = ["Hello", "world!"]
+ft_tuple = ("Hello", "Switzerland!")
+ft_set = {"Hello", "Lausanne!"}
+ft_dict = {"Hello" : "42Lausanne!"}
+
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
